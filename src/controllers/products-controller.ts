82d85d54c17e209312173acb2b1169b685e91454
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-02-21 23:24:07 -0300

    feat: Adiciona a rota de atualizar os produtos.

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -7,8 +7,8 @@ class ProductsController {
     try {
       const { name } = request.query;
       const products = await knex<ProductRepository>("products")
-      .whereLike("name", `%${name ?? ""}%`).select()
-      
+        .whereLike("name", `%${name ?? ""}%`).select()
+
       return response.json({ products })
     } catch (error) {
       next(error)
@@ -24,9 +24,29 @@ class ProductsController {
 
       const { name, price } = bodySchema.parse(request.body)
 
-      await knex<ProductRepository>("products").insert({ name, price})
+      await knex<ProductRepository>("products").insert({ name, price })
 
       response.status(201).json()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+
+      const id = z.string().transform((value) => Number(value)).refine((value) => !isNaN(value), { message: "Id must be a number" }).parse(request.params.id)
+
+      const bodySchema = z.object({
+        name: z.string({ required_error: "name is required" }).trim().min(6).optional(),
+        price: z.number({ required_error: "price is required" }).gt(0, { message: "value must be greater than 0" }).optional()
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      await knex<ProductRepository>("products").update({ name, price, updated_at: knex.fn.now() }).where({ id })
+
+      return response.status(200).json({ id, message:"Product updated."})
     } catch (error) {
       next(error)
     }

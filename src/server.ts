1ad3f84473e763017694895c58b0e359c60442b6
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-02-21 21:22:34 -0300

    feat: Adiciona estrutura inicial do projeto.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express"
+import { routes } from "./routes";
 
 const PORT = 3333;
 
 const app = express();
 app.use(express.json())
+
+app.use(routes)
 
 app.listen(PORT, ()=> console.log(`Servidor rodando na porta: ${PORT}`))
